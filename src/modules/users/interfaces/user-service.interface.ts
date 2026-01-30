d8fda3685d91b16cc89bf3c3c05ebc3651728698
vicente <matheus31102001@gmail.com>
2026-01-30 15:03:11 -0300

    created user service interface

---
+++ src/modules/users/interfaces/user-service.interface.ts
@@ -0,0 +1,11 @@
+import type { CreateUserDTO } from "../DTOs/create-user.dto";
+import type { UpdateUserDTO } from "../DTOs/update-user.dto";
+import type { UserResponseDTO } from "../DTOs/user-response.dto";
+
+export interface IUserService {
+    create(data: CreateUserDTO): Promise<UserResponseDTO> 
+    findById(id:string): Promise<UserResponseDTO | null>
+    findByUsername(username: string): Promise<UserResponseDTO | null>
+    update(id: string, data: UpdateUserDTO): Promise<UserResponseDTO>
+    softDelete(id: string): Promise<void>
+}
