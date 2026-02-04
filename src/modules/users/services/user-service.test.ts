5b129a12515600022e0bcd4020603cb9ff68ae19
vicente matheus <matheus31102001@gmail.com>
2026-02-03 22:29:51 -0300

    created findByUsername tests

--- src/modules/users/services/user-service.test.ts
+++ src/modules/users/services/user-service.test.ts
@@ -1,174 +1,202 @@
-import type { User } from "@prisma/client"
-import type { IHashUtils } from "../../hash/interfaces/hash-utils.interface"
-import type { IUserRepository } from "../interfaces/user-repository.interface"
-import { UserService } from "./user.service"
-import { ValidationError } from "../../../shared/errors/validation-error"
+import type { User } from "@prisma/client";
+import type { IHashUtils } from "../../hash/interfaces/hash-utils.interface";
+import type { IUserRepository } from "../interfaces/user-repository.interface";
+import { UserService } from "./user.service";
+import { ValidationError } from "../../../shared/errors/validation-error";
 
-describe('user service tests', () => {
-    const makeUser = (overrides?: Partial<User>): User => ({
-        id: "1",
+describe("user service tests", () => {
+  const makeUser = (overrides?: Partial<User>): User => ({
+    id: "1",
+    name: "Test",
+    username: "test",
+    role: "USER",
+    birthDate: new Date("2000-01-01"),
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    password: "hashed password",
+    deletedAt: null,
+    ...overrides,
+  });
+
+  const repositoryMock: jest.Mocked<IUserRepository> = {
+    create: jest.fn(),
+    findById: jest.fn(),
+    findByUsername: jest.fn(),
+    update: jest.fn(),
+    updatePassword: jest.fn(),
+    softDelete: jest.fn(),
+  };
+
+  const hashMock: jest.Mocked<IHashUtils> = {
+    hashPassword: jest.fn(),
+    comparePassword: jest.fn(),
+  };
+
+  const service = new UserService(repositoryMock, hashMock);
+
+  describe("create tests", () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it("should create a user successfully", async () => {
+      const mockUserReturn = makeUser();
+      repositoryMock.findByUsername.mockResolvedValue(null);
+      hashMock.hashPassword.mockResolvedValue("hashed password");
+
+      repositoryMock.create.mockResolvedValue(mockUserReturn);
+
+      const result = await service.create({
         name: "Test",
         username: "test",
-        role: "USER",
-        birthDate: new Date("2000-01-01"),
-        createdAt: new Date(),
-        updatedAt: new Date(),
+        password: "123",
+        birthDate: "01/01/2000",
+      });
+
+      expect(result).not.toHaveProperty("password");
+      expect(result).not.toHaveProperty("deletedAt");
+      expect(result).toHaveProperty("id");
+      expect(result).toHaveProperty("name");
+      expect(result).toHaveProperty("username");
+      expect(result).toHaveProperty("role");
+      expect(result).toHaveProperty("birthDate");
+      expect(result).toHaveProperty("createdAt");
+      expect(result).toHaveProperty("updatedAt");
+      expect(hashMock.hashPassword).toHaveBeenCalledWith("123");
+      expect(repositoryMock.create).toHaveBeenCalledWith({
+        name: "Test",
+        username: "test",
         password: "hashed password",
-        deletedAt: null,
-        ...overrides
-    })
+        birthDate: new Date(Date.UTC(2000, 0, 1)),
+      });
+    });
 
-    const repositoryMock: jest.Mocked<IUserRepository> = {
-        create: jest.fn(),
-        findById: jest.fn(),
-        findByUsername: jest.fn(),
-        update: jest.fn(),
-        updatePassword: jest.fn(),
-        softDelete: jest.fn()
-    }
+    it("should return an error because the username already exists", async () => {
+      repositoryMock.findByUsername.mockResolvedValue(makeUser());
 
-    const hashMock: jest.Mocked<IHashUtils> = {
-        hashPassword: jest.fn(),
-        comparePassword: jest.fn()
-    }
+      const user = service.create({
+        name: "test",
+        username: "test",
+        password: "test",
+        birthDate: "11/11/2000",
+      });
 
-    const service = new UserService(repositoryMock, hashMock)
+      expect(hashMock.hashPassword).not.toHaveBeenCalled();
+      await expect(user).rejects.toBeInstanceOf(ValidationError);
+      await expect(user).rejects.toThrow("Insira outro nome de usuário");
+      expect(repositoryMock.create).not.toHaveBeenCalled();
+    });
 
-    describe('create tests', () => {
+    it("should return an error because the birthdate is invaid", async () => {
+      repositoryMock.findByUsername.mockResolvedValue(null);
+      hashMock.hashPassword.mockResolvedValue("hashed password");
 
-        beforeEach(() => {
-            jest.clearAllMocks()
-        })
+      const result = service.create({
+        name: "Test",
+        username: "test",
+        password: "123",
+        birthDate: "31/02/2000",
+      });
 
-        it('should create a user successfully', async () => {
-            const mockUserReturn = makeUser()
-            repositoryMock.findByUsername.mockResolvedValue(null)
-            hashMock.hashPassword.mockResolvedValue('hashed password')
+      expect(repositoryMock.create).not.toHaveBeenCalled();
+      await expect(result).rejects.toThrow("Data inválida!");
+      await expect(result).rejects.toBeInstanceOf(ValidationError);
+    });
 
-            repositoryMock.create.mockResolvedValue(mockUserReturn);
+    it("should return an error because the birthdate is later than the current date", async () => {
+      repositoryMock.findByUsername.mockResolvedValue(null);
+      hashMock.hashPassword.mockResolvedValue("hashed password");
 
-            const result = await service.create({
-                name: "Test",
-                username: "test",
-                password: "123",
-                birthDate: '01/01/2000'
-            })
+      const year = new Date().getFullYear() + 1;
 
-            expect(result).not.toHaveProperty('password')
-            expect(result).not.toHaveProperty('deletedAt')
-            expect(result).toHaveProperty('id')
-            expect(result).toHaveProperty('name')
-            expect(result).toHaveProperty('username')
-            expect(result).toHaveProperty('role')
-            expect(result).toHaveProperty('birthDate')
-            expect(result).toHaveProperty('createdAt')
-            expect(result).toHaveProperty('updatedAt')
-            expect(hashMock.hashPassword).toHaveBeenCalledWith('123')
-            expect(repositoryMock.create).toHaveBeenCalledWith({
-                name: "Test",
-                username: "test",
-                password: "hashed password",
-                birthDate: new Date(Date.UTC(2000, 0, 1)),
-            });
+      const result = service.create({
+        name: "Test",
+        username: "test",
+        password: "123",
+        birthDate: `01/01/${year}`,
+      });
 
+      expect(repositoryMock.create).not.toHaveBeenCalled();
+      await expect(result).rejects.toThrow(
+        "Data de nascimento maior que a data atual",
+      );
+      await expect(result).rejects.toBeInstanceOf(ValidationError);
+    });
+  });
 
+  describe("findById tests", () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
 
+    it("should find a user sucessfully", async () => {
+      const user = makeUser();
+      repositoryMock.findById.mockResolvedValue(user);
 
+      const userTest = await service.findById(user.id);
 
-        })
+      expect(userTest).not.toBeNull();
+      expect(userTest).not.toHaveProperty("password");
+      expect(userTest).not.toHaveProperty("deletedAt");
+      expect(userTest).toHaveProperty("createdAt");
+      expect(userTest).toHaveProperty("updatedAt");
+      expect(repositoryMock.findById).toHaveBeenCalledWith(user.id);
+      expect(repositoryMock.findById).toHaveBeenCalledTimes(1);
+      expect(userTest).toMatchObject({
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        role: user.role,
+        birthDate: user.birthDate,
+      });
+    });
 
-        it('should return an error because the username already exists', async () => {
-            repositoryMock.findByUsername.mockResolvedValue(makeUser())
+    it("should return null because it cannot find a user with the given ID", async () => {
+      repositoryMock.findById.mockResolvedValue(null);
 
-            const user = service.create({
-                name: "test",
-                username: "test",
-                password: "test",
-                birthDate: "11/11/2000"
-            })
+      const userTest = await service.findById("123");
 
-            expect(hashMock.hashPassword).not.toHaveBeenCalled()
-            await expect(user).rejects.toBeInstanceOf(ValidationError)
-            await expect(user).rejects.toThrow('Insira outro nome de usuário')
-            expect(repositoryMock.create).not.toHaveBeenCalled()
-        })
+      expect(userTest).toBeNull();
+      expect(repositoryMock.findById).toHaveBeenCalledWith("123");
+      expect(repositoryMock.findById).toHaveBeenCalledTimes(1);
+    });
+  });
 
-        it("should return an error because the birthdate is invaid", async () => {
-            repositoryMock.findByUsername.mockResolvedValue(null)
-            hashMock.hashPassword.mockResolvedValue('hashed password')
+  describe("findByUsername tests", () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
 
-            const result = service.create({
-                name: "Test",
-                username: "test",
-                password: "123",
-                birthDate: '31/02/2000'
-            })
+    it("should find a user sucessfully", async () => {
+      const user = makeUser();
+      repositoryMock.findByUsername.mockResolvedValue(user);
 
-            expect(repositoryMock.create).not.toHaveBeenCalled()
-            await expect(result).rejects.toThrow('Data inválida!')
-            await expect(result).rejects.toBeInstanceOf(ValidationError)
+      const userTest = await service.findByUsername(user.username);
 
+      expect(repositoryMock.findByUsername).toHaveBeenCalledTimes(1);
+      expect(repositoryMock.findByUsername).toHaveBeenCalledWith(user.username);
 
-        })
+      expect(userTest).toEqual(
+        expect.objectContaining({
+          id: expect.any(String),
+          name: expect.any(String),
+          username: expect.any(String),
+          role: expect.any(String),
+          birthDate: expect.any(Date),
+          createdAt: expect.any(Date),
+          updatedAt: expect.any(Date),
+        }),
+      );
+    });
 
-        it("should return an error because the birthdate is later than the current date", async () => {
-            repositoryMock.findByUsername.mockResolvedValue(null)
-            hashMock.hashPassword.mockResolvedValue('hashed password')
+    it("should return null because it cannot find a user with the given username", async () => {
+      repositoryMock.findByUsername.mockResolvedValue(null);
 
-            const year = new Date().getFullYear() + 1
+      const userTest = await service.findByUsername("username");
 
-            const result = service.create({
-                name: "Test",
-                username: "test",
-                password: "123",
-                birthDate: `01/01/${year}`
-            })
-
-            expect(repositoryMock.create).not.toHaveBeenCalled()
-            await expect(result).rejects.toThrow('Data de nascimento maior que a data atual')
-            await expect(result).rejects.toBeInstanceOf(ValidationError)
-
-        })
-
-
-    })
-
-    describe('findById tests', () => {
-        beforeEach(() => {
-            jest.clearAllMocks()
-        })
-
-        it('should find a user sucessfully', async () => {
-            const user = makeUser()
-            repositoryMock.findById.mockResolvedValue(user)
-
-            const userTest = await service.findById(user.id)
-
-            expect(userTest).not.toBeNull()
-            expect(userTest).not.toHaveProperty('password')
-            expect(userTest).not.toHaveProperty('deletedAt')
-            expect(userTest).toHaveProperty('createdAt')
-            expect(userTest).toHaveProperty('updatedAt')
-            expect(repositoryMock.findById).toHaveBeenCalledWith(user.id)
-            expect(repositoryMock.findById).toHaveBeenCalledTimes(1)
-            expect(userTest).toMatchObject({
-                id: user.id,
-                name: user.name,
-                username: user.username,
-                role: user.role,
-                birthDate: user.birthDate,
-            })
-        })
-
-        it('should return null because it cannot find a user with the given ID', async () => {
-            repositoryMock.findById.mockResolvedValue(null)
-
-            const userTest = await service.findById('123')
-
-            expect(userTest).toBeNull()
-            expect(repositoryMock.findById).toHaveBeenCalledWith('123')
-            expect(repositoryMock.findById).toHaveBeenCalledTimes(1)
-        })
-    })
-
-})
+      expect(repositoryMock.findByUsername).toHaveBeenCalledTimes(1);
+      expect(repositoryMock.findByUsername).toHaveBeenCalledWith("username");
+      expect(userTest).toBeNull();
+    });
+  });
+});
