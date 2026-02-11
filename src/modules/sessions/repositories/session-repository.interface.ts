96f1b8cc9c74724ed31e4f4df1f3641b44fe9844
vicente matheus <matheus31102001@gmail.com>
2026-02-10 21:08:51 -0300

    created createSessionDTO and ISessionRepository

---
+++ src/modules/sessions/repositories/session-repository.interface.ts
@@ -0,0 +1,12 @@
+import type { Session } from "@prisma/client";
+import type { CreateSessionDTO } from "../DTOs/create-session.dto";
+
+export interface ISessionRepository {
+    create(data: CreateSessionDTO): Promise<Session>
+
+    findByRefreshToken(refreshToken: string): Promise<Session | null>
+
+    findByUserId(id: string): Promise<Session | null>
+
+    invalidate(sessionId: string): Promise<void>
+}
